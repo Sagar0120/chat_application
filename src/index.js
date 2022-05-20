42eddca54d895e95a54f96e5e0952800c1dd3ccd
Sagar <sagarbhanushali0120@gmail.com>
2022-05-20 14:08:12 +0530

    firts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdvCQHkYv2HRt0MVzZ0h0dBZH69a9g30U",
+  authDomain: "react-chat-app-87a6c.firebaseapp.com",
+  databaseURL: "https://react-chat-app-87a6c-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-87a6c",
+  storageBucket: "react-chat-app-87a6c.appspot.com",
+  messagingSenderId: "414507514140",
+  appId: "1:414507514140:web:3e587510d8e403386e22d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
