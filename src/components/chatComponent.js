ead169aa05f01be0ac7717701e3c3a20843f6f5d
Sagar <sagarbhanushali0120@gmail.com>
2022-05-21 09:36:16 +0530

    add component

--- src/components/chatComponent.js
+++ src/components/chatComponent.js
@@ -1,111 +1,119 @@
-import { useEffect, useState } from 'react';
-import { getDatabase, push, ref, set,onChildAdded } from "firebase/database";
+import { useEffect, useState } from "react";
+import { getDatabase, push, ref, set, onChildAdded } from "firebase/database";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
-import './App.css';
-
-
 export default function chatComponent() {
-  return (
-    <div>
-      const provider = new GoogleAuthProvider();
-      const auth = getAuth();
-
-      const googleLogin = () =>{
-        signInWithPopup(auth, provider)
-        .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    setUser({name:result.user.displayName, email: result.user.email})
-    console.log(token, user);
-
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    console.log("error : " + errorCode);
-    const errorMessage = error.message;
-    console.log("error message : " + errorMessage)
-    // The email of the user's account used.
-    const email = error.email;
-    console.log("email : " + email);
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log("credential : " + credential);
-    // ...
-  });
-  }
-
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [chats, setChats] = useState([]);
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState("");
 
   const db = getDatabase();
-  const chatListRef = ref(db, 'chats');
+  const chatListRef = ref(db, "chats");
 
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
 
+  const googleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        setUser({ name: result.user.displayName, email: result.user.email });
+        console.log(token, user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        console.log("error : " + errorCode);
+        const errorMessage = error.message;
+        console.log("error message : " + errorMessage);
+        // The email of the user's account used.
+        const email = error.email;
+        console.log("email : " + email);
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log("credential : " + credential);
+        // ...
+      });
+  };
 
-  const updateHeight=()=>{
-    const el = document.getElementById('chat');
-    if(el){
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
       el.scrollTop = el.scrollHeight;
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     onChildAdded(chatListRef, (data) => {
-      setChats(chats=>[...chats,data.val()])
-      setTimeout(()=>{
-        updateHeight()
-      },100)
+      setChats((chats) => [...chats, data.val()]);
+      setTimeout(() => {
+        updateHeight();
+      }, 100);
     });
-  },[])
-
+  }, []);
 
   const sendChat = () => {
-
     const chatRef = push(chatListRef);
     set(chatRef, {
-      user, message: msg 
+      user,
+      message: msg,
     });
-    setMsg('');
+    setMsg("");
   };
+
   return (
-    <div>
-      {user.email? null: <div>
-        {/* <input
+    <>
+      <div>
+        {user.email ? null : (
+          <div>
+            {/* <input
           type="text"
           placeholder="Enter user to start"
           onBlur={(e) => setUser(e.target.value)}
         ></input> */}
-        <button onClick={e=>{googleLogin()}}>Google SignIn</button>
-      </div>}
-   { user.email? <div>
-      <h3>User: {user.name}</h3>
-      <div id="chat" className="chat-container">
-        {chats.map((c,i) => (
-          <div key={i} className={`container ${c.user.email === user.email ? 'me' : ''}`}>
-            <p className="chatbox">
-              <strong>{c.user.name}: </strong>
-              <span>{c.message}</span>
-            </p>
+            <button
+              onClick={(e) => {
+                googleLogin();
+              }}
+            >
+              Google SignIn
+            </button>
           </div>
-        ))}
+        )}
+        {user.email ? (
+          <div>
+            <h3>User: {user.name}</h3>
+            <div id="chat" className="chat-container">
+              {chats.map((c, i) => (
+                <div
+                  key={i}
+                  className={`container ${
+                    c.user.email === user.email ? "me" : ""
+                  }`}
+                >
+                  <p className="chatbox">
+                    <strong>{c.user.name}: </strong>
+                    <span>{c.message}</span>
+                  </p>
+                </div>
+              ))}
+            </div>
+            <div className="btm">
+              <input
+                type="text"
+                onInput={(e) => setMsg(e.target.value)}
+                value={msg}
+                placeholder="enter your chat"
+              ></input>
+              <button onClick={(e) => sendChat()}>Send</button>
+            </div>
+          </div>
+        ) : null}
       </div>
-      <div className="btm">
-        <input
-          type="text"
-          onInput={(e) => setMsg(e.target.value)}
-          value={msg}
-          placeholder="enter your chat"
-        ></input>
-        <button onClick={(e) => sendChat()}>Send</button>
-      </div>
-      </div> : null}
-      </div>
-    </div>
-
-  )
+    </>
+  );
 }
